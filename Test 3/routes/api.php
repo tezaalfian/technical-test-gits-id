6bd74ee5168834240e1383e2023084f5b32337fe
Teza Alfian <44139001+tezaalfian@users.noreply.github.com>
2023-01-12 06:51:49 +0700

    change base route view

--- Test 3/routes/api.php
+++ Test 3/routes/api.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Welcome to the BOOKS API'
+    ]);
+});
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::group([
     'middleware' => 'auth.token'
